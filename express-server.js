027e94ba1a333d28917a8ae83a3375ff72f14edc
khuram <knshaikh@gmail.comgit config --global user.email knshaikh@gmail.comgit config --global user.email knshaikh@gmail.com>
2017-05-12 00:13:26 +0000

    display list uring ejs template

---
+++ express-server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+app.set("view engine", "ejs");
+
+var PORT = process.env.PORT || 8080; //default port 8080
+
+var urlDatabase = {
+  "b2xVn2" : "www.lighthouselabs.ca",
+  "9sm5sK" : "www.google.com"
+}
+
+app.get("/", (req, res) => {
+  res.end("Hello! Khuram");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = {urls : urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello! <b>World</b></body></html>")
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listing to port ${PORT}!`);
+});
