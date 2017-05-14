4d54731f7cc474adf45a727882a098b6f5cfe0bc
khuram <knshaikh@gmail.comgit config --global user.email knshaikh@gmail.comgit config --global user.email knshaikh@gmail.com>
2017-05-14 20:49:27 +0000

    Created the registration handler

--- express-server.js
+++ express-server.js
@@ -1,23 +1,50 @@
-var express = require("express");
-var cookieParser = require('cookie-parser')
-var app = express();
+const express = require("express");
+const cookieParser = require('cookie-parser')
+const app = express();
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser())
 
-var PORT = process.env.PORT || 8080; //default port 8080
+const PORT = process.env.PORT || 8080; //default port 8080
 
-var urlDatabase = {
+//URL Object to be used as a data store
+const urlDatabase = {
   "b2xVn2" : "http://www.lighthouselabs.ca",
   "9sm5sK" : "http://www.google.com"
+}
+
+//Users Object to be used as a users database
+const userDatabase = {
+  "userRandomId": {
+    id: "userRandomId",
+    email: "user1@example.com",
+    password: "654321",
+  },
+
+  "user2": {
+    id: "user2",
+    email: "user2@example.com",
+    password: "123456"
+  },
+
+  "user3": {
+    id: "user3",
+    email: "user3@example.com",
+    password: "1456"
+  },
+  "user4": {
+    id: "user4",
+    email: "user4@example.com",
+    password: "12346"
+  },
 }
 
 //Generate random string of 6 alpha numeric characters
 function generateRandomString() {
 
-  var url_id = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let url_id = "";
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   for (var i = 0; i < 6; i++) {
     url_id += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -26,7 +53,7 @@ function generateRandomString() {
 }
 
 app.get("/", (req, res) => {
-  res.end("Hello! Khuram");
+  res.end("Please check the url you need to acess!");
 });
 
 //Render endpoint urls_index from /urls in the address bar
@@ -36,6 +63,42 @@ app.get("/urls", (req, res) => {
     username: req.cookies["username"]
   };
   res.render("urls_index", templateVars);
+});
+
+//Register a user
+app.get("/register", (req, res) => {
+  //pass the variables to regiter ejs template
+  res.render("urls_register");
+});
+
+//Registeration handler Post
+app.post("/register", (req, res) => {
+
+  for (var index in userDatabase) {
+    console.log(userDatabase[index].email);
+    if (req.body.email === userDatabase[index].email) {
+      res.status(400).send("You are already registered with this email!");
+
+    }
+    else if (!(req.body.email && req.body.password)) {
+      res.status(400).send("Please enter a valid email or password!");
+
+    }
+
+    else {
+      let userId = generateRandomString();
+      userDatabase[userId] = {
+        id: userId,
+        email:  req.body.email,
+        password:  req.body.password
+      }
+      res.cookie('username', userId);
+      //console.log(userDatabase)
+      res.redirect("/urls");
+    }
+
+  }
+
 });
 
 //Get new URL
@@ -60,7 +123,7 @@ app.get("/urls/:id", (req, res) => {
 //Add a new URL post
 app.post("/urls", (req, res) => {
   console.log(req.body.longURL); //debug statement to see post parameters
-  shortURL = generateRandomString(); //generate random string and assign to shortURL
+  let shortURL = generateRandomString(); //generate random string and assign to shortURL
   urlDatabase[shortURL] = req.body.longURL; //pass the shortURL and longURL to urlDatabase
       res.redirect(urlDatabase[shortURL]);
 });
